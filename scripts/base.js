bba269e5ec09a707d3ff67183ca85764d4d106eb
Sazungen <sasungen@gmail.com>
2025-03-10 21:06:54 +0100

    save dark mode between pages, apply dark mode automatically if enabled in browser

--- scripts/base.js
+++ scripts/base.js
@@ -1,8 +1,32 @@
 /* Viktor Högberg, Léo Tuomenoksa Texier */
+window.onload = initialise;
+
+//runs at the start of every document load and checks if dark mode and sound has been set by the user
+//and activates toggles them if they have been set
+function initialise() {
+    if (localStorage.getItem('theme') == "dark") {
+        document.documentElement.classList.add("dark-mode");
+        //no need to keep going since dark mode has already been set
+        return;
+    }
+
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && localStorage.getItem('theme') != "light") {
+        localStorage.setItem('theme', 'dark');
+        document.documentElement.classList.add("dark-mode");
+    }
+}
 
 // Switches between light and dark mode
 document.getElementById("theme-toggle-button").addEventListener("click", () => {
-    document.documentElement.classList.toggle("dark-mode");
+    //if dark mode is active and user clicks theme button, remove dark mode and set current theme to light
+    if (localStorage.getItem('theme') == "dark") {
+        localStorage.setItem('theme', 'light');
+        document.documentElement.classList.remove("dark-mode");
+    } else {
+        localStorage.setItem('theme', 'dark');
+        document.documentElement.classList.add("dark-mode");
+    }
+
 })
 
 // When clicking on the "SortingLab"  logo,
