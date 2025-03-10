7ff527ee97071187efc4c11876793acb7e7200ea
vhogberg <119532259+vhogberg@users.noreply.github.com>
2025-03-10 09:40:56 +0100

    added cookie support for mute switch
    Co-authored-by: Sazungen <Sazungen@users.noreply.github.com>

--- scripts/base.js
+++ scripts/base.js
@@ -1,7 +1,5 @@
 /* Viktor Högberg, Léo Tuomenoksa Texier */
 
-import { muteSound, unMuteSound } from "./sound.js";
-
 // Switches between light and dark mode
 document.getElementById("theme-toggle-button").addEventListener("click", () => {
     document.documentElement.classList.toggle("dark-mode");
@@ -11,14 +9,3 @@ document.getElementById("theme-toggle-button").addEventListener("click", () => {
 document.getElementById("logo-container").addEventListener("click", () => {
     window.location.href = "index.html";
 })
-
-const soundChechbox = document.getElementById("sound-checkbox");
-// if sound checkbox is checked, play sound, else mute it
-soundChechbox.addEventListener("change", function() {
-    if (this.checked) {
-        unMuteSound();
-    }
-    else {
-        muteSound();
-    }
-});
--- scripts/sound.js
+++ scripts/sound.js
@@ -1,21 +1,18 @@
 /* Viktor Högberg, Léo Tuomenoksa Texier */
 
-// SOUND EFFECT HANDLING
+// ------------------------- SOUND EFFECTS -----------------------------
 
 // list of sounds
-let errorSound = new Audio('../assets/sound-effects/error-sound-effect.mp3');
-let tickingSound = new Audio('../assets/sound-effects/ticking-sound-effect.mp3');
-let gameOverSuccessSound = new Audio('../assets/sound-effects/game-over-success-sound-effect.mp3');
-let gameOverFailSound = new Audio('../assets/sound-effects/game-over-fail-sound-effect.mp3');
+let errorSound = new Audio('./assets/sound-effects/error-sound-effect.mp3');
+let tickingSound = new Audio('./assets/sound-effects/ticking-sound-effect.mp3');
+let gameOverSuccessSound = new Audio('./assets/sound-effects/game-over-success-sound-effect.mp3');
+let gameOverFailSound = new Audio('./assets/sound-effects/game-over-fail-sound-effect.mp3');
 
 // volume control
 errorSound.volume = 0.03;
 gameOverSuccessSound.volume = 0.15;
 gameOverFailSound.volume = 0.03;
 tickingSound.volume = 0.05;
-
-// mute control
-let isMuted = false;
 
 // play functions
 export function playErrorSound() {
@@ -43,13 +40,62 @@ export function playGameOverFailSound() {
     }
 }
 
+// ------------------------- MUTE CONTROL ------------------------------
+
+let isMuted = getCookie("muteSound") === "true";
+
+const soundCheckbox = document.getElementById("sound-checkbox");
+
+// if sound checkbox is checked, play sound, else mute it
+soundCheckbox.addEventListener("change", function() {
+    if (this.checked) {
+        unMuteSound();
+    }
+    else {
+        muteSound();
+    }
+});
+
+function updateMutedCheckbox() {
+    soundCheckbox.checked = !isMuted;
+}
+
+function setCookie(name, value, days) {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+    document.cookie = name + "=" + value + "; expires=" + expires.toUTCString() + ";path=/";
+}
+
+function getCookie(name) {
+    let cookieName = name + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let characterArray = decodedCookie.split(';');
+    
+    for (let i = 0; i < characterArray.length; i++){
+        let char = characterArray[i];
+        while (char.charAt(0) == ' ') {
+            char = char.substring(1);
+        }
+        if(char.indexOf(name) == 0) {
+            return char.substring(cookieName.length, char.length);
+        }
+        else return "";
+    }
+}
+
 // mute functions
 export function muteSound () {
     isMuted = true;
+    setCookie("muteSound", "true", 7); // 7 days
+    updateMutedCheckbox();
+    console.log("cookies: "+document.cookie);
 }
 
 export function unMuteSound () {
     isMuted = false;
+    setCookie("muteSound", "false", 7); // 7 days
+    updateMutedCheckbox();
+    console.log("cookies: "+document.cookie);
 }
 
 export function isSoundMuted() {
